195bd7ab924bdb80f7f3ea8777599dc8a50c756c
BMSales <bruno.m.sales03@gmail.com>
2026-07-22 02:41:45 -0300

    Added TODO list

--- linear-algebra.hpp
+++ linear-algebra.hpp
@@ -18,3 +18,19 @@ private:
 	int cols;
 	std::vector<std::vector<float>> values;
 };
+
+// TODO:
+// Add the following operations
+// determinant
+// transpose
+// inverse
+// gauss jordan shit
+// addition and subtraction, if that's a thing
+// autovalor e autovetor (not sure)
+
+// TODO:
+// Add the following functionalities
+// GetRows
+// GetCollumns
+// SwapRows
+// SwapCollumns
